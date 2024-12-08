a5f8469ccf92808145c2e8615bb4503b5804939c
Emmanueloye <esunday963@gmail.com>
2024-12-08 13:46:33 +0100

    update

--- backend/build/utils/helperFunctions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.slugifyInput = void 0;
-const slugifyInput = (str) => {
-    return str.toLowerCase().split(' ').join('-');
-};
-exports.slugifyInput = slugifyInput;
